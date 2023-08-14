296ec49d60ff7a24df618bddd8fb8ef7c43e1a3f
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2023-08-14 18:13:24 +0530

    assignment

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+ 
+class Post(models.Model):
+    username = models.CharField(max_length=50)
+    caption = models.TextField()
+    created_al = models.DateTimeField(auto_now_add = True)
+    likes = models.PositiveIntegerField(default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
